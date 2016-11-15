28a3a897f6aa65925450552b10f3f51784ade523
Valentina Henao <valen410@gmail.com>
2016-11-15 12:42:02 -0500

    complete UI, all functioning

---
+++ BlissApp/HeaderTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderTableViewCell.swift
+//  BlissApp
+//
+//  Created by Valentina Henao on 11/9/16.
+//  Copyright © 2016 Valentina Henao. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
