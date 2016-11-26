5ce6624ab54a4bba2568597021a8dfc1c8d8a46c
Valentina Henao <valen410@gmail.com>
2016-11-26 16:29:36 -0500

    Changed images and layouts

---
+++ BlissApp/settingsTableVIewTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  settingsTableVIewTableViewCell.swift
+//  BlissApp
+//
+//  Created by Valentina Henao on 11/25/16.
+//  Copyright © 2016 Valentina Henao. All rights reserved.
+//
+
+import UIKit
+
+class settingsTableVIewTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
